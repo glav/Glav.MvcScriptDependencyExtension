db9d2833118a4d37ac64018e4efa16a5b37613a6
Paul Glavich <glav@theglavs.com>
2010-11-08 07:12:43 +1100

    Woops. Forgot this file.

---
+++ ScriptDependencyExtension/ScriptState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScriptDependencyExtension
+{
+    internal enum ScriptState
+    {
+        Original,  // neither debug nor release, but its original naming
+        Debug,     // the script is named with a debug extension eg. script.debug.js
+        Release    // the script is named with a release extension  eg. script.min.js
+    }
+}
