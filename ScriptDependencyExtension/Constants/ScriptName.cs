4bc84bdcb13699ab59598a4bccc76a3c44f467e9
Paul Glavich <glav@theglavs.com>
2010-11-08 18:39:11 +1100

    Amended namespaceof ScriptName class and amended example code

--- ScriptDependencyExtension/Constants/ScriptName.cs
+++ ScriptDependencyExtension/Constants/ScriptName.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ScriptDependencyExtension.Constants
+namespace ScriptDependencyExtension
 {
     public class ScriptName
     {
