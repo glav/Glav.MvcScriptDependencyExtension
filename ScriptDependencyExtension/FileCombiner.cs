4d211e01b736adb66b9e5d9a8421bb6c11f3d3d4
Paul Glavich <glav@theglavs.com>
2011-03-29 09:50:20 +1100

    Added FileCombiner and tests. Also added in AjaxMin (MSFT Ajax Minifier) as dependency

---
+++ ScriptDependencyExtension/FileCombiner.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using ScriptDependencyExtension.Http;
+
+namespace ScriptDependencyExtension
+{
+	public class FileCombiner
+	{
+		private List<string> _filesToCombine = new List<string>();
+		private IHttpContext _httpContext;
+
+		public FileCombiner(IHttpContext context)
+		{
+			_httpContext = context;
+		}
+		public FileCombiner(IHttpContext context, IEnumerable<string> filessToCombine) : this(context) 
+		{
+			_filesToCombine.AddRange(filessToCombine);	
+		}
+
+		public void AddFileToCombine(string filename)
+		{
+			if (!_filesToCombine.Contains(filename))
+				_filesToCombine.Add(filename);
+		}
+
+		public FileCombinationResult CombineFiles(string fileExtension)
+		{
+			var result = new FileCombinationResult();
+
+			result.CmbinedFileContents = CombineFileContents();
+			result.CombinedFileName = GenerateFileName(fileExtension);
+
+			return result;
+		}
+
+		private string GenerateFileName(string fileExtension)
+		{
+			var realExtension = new StringBuilder();
+			if (!string.IsNullOrWhiteSpace(fileExtension))
+			{
+				if (fileExtension[0] != '.')
+				{
+					realExtension.Append('.');
+				}
+				realExtension.Append(fileExtension);
+			}
+
+			var nameBuilder = new StringBuilder();
+			foreach (var filename in _filesToCombine)
+			{
+				nameBuilder.Append(filename);
+			}
+			//TODO: Cannot use hashcode here as it uses different numbers for same filenames but different instances
+			return string.Format("Resource-{0}.combined{1}", nameBuilder.GetHashCode(),realExtension.ToString());
+		}
+
+		private string CombineFileContents()
+		{
+			var fileContents = new StringBuilder();
+			foreach (var filename in _filesToCombine)
+			{
+				var fileData = File.ReadAllText(_httpContext.ResolveScriptRelativePath(filename));
+				fileContents.Append(fileData);
+			}
+			return fileContents.ToString();
+		}
+	}
+
+	public class FileCombinationResult
+	{
+		public string CombinedFileName { get; set; }
+		public string CmbinedFileContents { get; set; }
+	}
+}
---
+++ ScriptDependencyTests/FileCombinationTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScriptDependencyExtension;
+using System.IO;
+using ScriptDependencyExtension.Http;
+
+namespace ScriptDependencyTests
+{
+	[TestClass]
+	public class FileCombinationTests
+	{
+		[TestMethod]
+		[DeploymentItem("TestFile1.txt")]
+		[DeploymentItem("TestFile2.txt")]
+		public void ShouldCombineFiles()
+		{
+			var fileCombiner = new FileCombiner(new HttpContextAdapter(), new string[] {"TestFile1.txt", "TestFile2.txt"});
+			var result = fileCombiner.CombineFiles("js");
+
+			Assert.IsNotNull(result);
+			Assert.IsFalse(string.IsNullOrWhiteSpace(result.CmbinedFileContents));
+			Assert.IsFalse(string.IsNullOrWhiteSpace(result.CombinedFileName));
+
+			// Make sure it contains the combined contents
+			Assert.IsTrue(result.CmbinedFileContents.Contains("This is testfile 1"));
+			Assert.IsTrue(result.CmbinedFileContents.Contains("This is testfile 2"));
+		}
+		
+		[TestMethod]
+		[DeploymentItem("TestFile1.txt")]
+		[DeploymentItem("TestFile2.txt")]
+		public void ShouldGenerateUniqueCombinedFileNames()
+		{
+			var fileCombiner1 = new FileCombiner(new HttpContextAdapter(), new string[] { "TestFile1.txt", "TestFile2.txt" });
+			var fileCombiner2 = new FileCombiner(new HttpContextAdapter(), new string[] { "TestFile1.txt" });
+			var fileCombiner3 = new FileCombiner(new HttpContextAdapter(), new string[] { "TestFile2.txt" });
+			var fileCombiner4 = new FileCombiner(new HttpContextAdapter(), new string[] { "TestFile1.txt", "TestFile2.txt" });
+			var result1 = fileCombiner1.CombineFiles("js");
+			var result2 = fileCombiner2.CombineFiles("js");
+			var result3 = fileCombiner3.CombineFiles("js");
+			var result4 = fileCombiner4.CombineFiles("js");
+
+			Assert.IsNotNull(result1);
+			Assert.IsNotNull(result2);
+			Assert.IsNotNull(result3);
+			Assert.IsNotNull(result4);
+
+			Assert.AreNotEqual<string>(result1.CombinedFileName, result2.CombinedFileName);
+			Assert.AreNotEqual<string>(result1.CombinedFileName, result3.CombinedFileName);
+			Assert.AreNotEqual<string>(result2.CombinedFileName, result3.CombinedFileName);
+
+			// These instances combined exactly the same content so the combined file names should
+			// be the same.
+			Assert.AreEqual<string>(result1.CombinedFileName,result4.CombinedFileName);
+		}
+	}
+}
