061ce7b889b086981df11b55792151ccae278fc9
Paul Glavich <glav@theglavs.com>
2011-04-04 19:11:41 +1000

    Reformatting

--- ScriptDependencyExtension/Handler/ScriptServeHandler.cs
+++ ScriptDependencyExtension/Handler/ScriptServeHandler.cs
@@ -9,7 +9,7 @@ using ScriptDependencyExtension.Helpers;
 
 namespace ScriptDependencyExtension.Handler
 {
-	public class FileServeHandler: IHttpHandler
+	public class ScriptServeHandler: IHttpHandler
 	{
 		bool IHttpHandler.IsReusable
 		{
@@ -19,12 +19,13 @@ namespace ScriptDependencyExtension.Handler
 		void IHttpHandler.ProcessRequest(HttpContext context)
 		{
 			IHttpContext contextAdaptor = new HttpContextAdapter(context);
-			ScriptDependencyLoader scriptLoader = new ScriptDependencyLoader(contextAdaptor);
+			var scriptLoader = new ScriptDependencyLoader(contextAdaptor);
 			scriptLoader.Initialise();
 
 			var engine = new ScriptEngine(contextAdaptor, new ScriptDependencyLoader(contextAdaptor));
 			var tokenHelper = new TokenisationHelper();
 			var dependencies = tokenHelper.GetListOfDependencyNamesFromQueryStringTokens(context.Request.RawUrl,scriptLoader.DependencyContainer);
+			
 			var scriptType = dependencies[0].TypeOfScript;
 			var contentType = ScriptHelperConstants.ContentType_Javascript;
 			if (dependencies.Count > 0)
@@ -32,10 +33,12 @@ namespace ScriptDependencyExtension.Handler
 				if (scriptType == ScriptType.CSS)
 					contentType = ScriptHelperConstants.ContentType_CSS;
 			}
+			
 			var listOfFiles = dependencies.Select(d => d.ScriptPath).ToList();
 			var combiner = new FileCombiner(contextAdaptor,listOfFiles);
 			StringBuilder contents = new StringBuilder(combiner.CombineFiles());
 			engine.ApplyFiltersToScriptOutput(contents, scriptType);
+			
 			context.Response.ContentType = contentType;
 			context.Response.Write(contents.ToString());
 		}
