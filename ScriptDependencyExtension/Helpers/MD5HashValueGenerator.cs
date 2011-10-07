203fd105a30bee2faf09cb124f0b4fb28c41e7b4
Paul Glavich <glav@theglavs.com>
2011-10-07 19:06:22 +1100

    Added extratest

--- ScriptDependencyExtension/Helpers/MD5HashValueGenerator.cs
+++ ScriptDependencyExtension/Helpers/MD5HashValueGenerator.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace ScriptDependencyExtension.Helpers
 {
+	//Note: This can be usedin place of the SimpleHashValueGenerator and will provide a much more
+	//      unique name, however the length of the names aremuch larger and may cause the resulting URL
+	//      using these unique names to be excessive.
 	public class MD5HashValueGenerator : IUniqueHashValueGenerator
 	{
 		public string ComputeHash(string input)
--- ScriptDependencyTests/TokenisationHelperTests.cs
+++ ScriptDependencyTests/TokenisationHelperTests.cs
@@ -62,5 +62,41 @@ namespace ScriptDependencyTests
 			Assert.AreEqual<string>("one", list[0].ScriptName);
 			Assert.AreEqual<string>("two", list[1].ScriptName);
 		}
+
+		[TestMethod]
+		public void ShouldGenerateUniqueTokensForVerySimilarStrings()
+		{
+			var helper = new TokenisationHelper();
+
+			const string baseCompareValue = "SomeName";
+
+			Assert.AreNotEqual<string>(helper.TokeniseString("1"), helper.TokeniseString("2"));
+			Assert.AreNotEqual<string>(helper.TokeniseString("11"), helper.TokeniseString("12"));
+			Assert.AreNotEqual<string>(helper.TokeniseString("111"), helper.TokeniseString("112"));
+			Assert.AreNotEqual<string>(helper.TokeniseString("111"), helper.TokeniseString("211"));
+			Assert.AreNotEqual<string>(helper.TokeniseString("11"), helper.TokeniseString("21"));
+			Assert.AreNotEqual<string>(helper.TokeniseString("---1"), helper.TokeniseString("----1"));
+			Assert.AreNotEqual<string>(helper.TokeniseString("22"), helper.TokeniseString("223"));
+			Assert.AreNotEqual<string>(helper.TokeniseString("12"), helper.TokeniseString("21"));
+
+			for (int i = 0; i < 100; i++)
+			{
+				var bareToken = i.ToString();
+				var compareBareToken = (i + 1).ToString();
+				var initialToken = string.Format("{0}__{1}", baseCompareValue, i);
+				var secondToken = string.Format("{0}__{1}{1}", baseCompareValue, i);
+				var thirdToken = string.Format("{0}_{1}", baseCompareValue, i);
+				StringBuilder reversedToken = new StringBuilder();
+				initialToken.Reverse().ToList().ForEach(c => reversedToken.Append(c));
+				Assert.AreNotEqual<string>(helper.TokeniseString(bareToken), helper.TokeniseString(compareBareToken));
+				Assert.AreNotEqual<string>(helper.TokeniseString(initialToken), helper.TokeniseString(secondToken));
+				Assert.AreNotEqual<string>(helper.TokeniseString(initialToken), helper.TokeniseString(secondToken));
+				Assert.AreNotEqual<string>(helper.TokeniseString(initialToken), helper.TokeniseString(thirdToken));
+				Assert.AreNotEqual<string>(helper.TokeniseString(secondToken), helper.TokeniseString(thirdToken));
+				Assert.AreNotEqual<string>(helper.TokeniseString(initialToken), helper.TokeniseString(reversedToken.ToString()));
+
+			}
+
+		}
 	}
 }
