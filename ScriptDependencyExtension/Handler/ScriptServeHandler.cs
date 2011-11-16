fe53e65e4611b48246df8724c85e5f544eaae756
Paul Glavich <glav@theglavs.com>
2011-11-16 13:19:57 +1100

    performance modifications

--- ScriptDependencyExtension/Handler/ScriptServeHandler.cs
+++ ScriptDependencyExtension/Handler/ScriptServeHandler.cs
@@ -12,20 +12,43 @@ namespace ScriptDependencyExtension.Handler
 {
 	public class ScriptServeHandler: IHttpHandler
 	{
+		private static ScriptEngine _scriptEngine;
+		private static IHttpContext _contextAdapter;
+		private static IScriptDependencyLoader _scriptLoader;
+		private static object _lockObject = new object();
+		private static ITokenisationHelper _tokenHelper;
+
 		bool IHttpHandler.IsReusable
 		{
 			get { return true; }
 		}
 
+		void EnsureHandlersAreInitialised(HttpContext context)
+		{
+			if (_scriptEngine != null && _contextAdapter != null && _scriptLoader != null && _tokenHelper != null)
+			{
+				return;
+			}
+
+			lock (_lockObject)
+			{
+				if (_scriptEngine == null || _contextAdapter == null || _scriptLoader == null || _tokenHelper == null)
+				{
+					_contextAdapter = new HttpContextAdapter(context);
+					_scriptLoader = new ScriptDependencyLoader(_contextAdapter);
+					_scriptLoader.Initialise();
+					_scriptEngine = new ScriptEngine(_contextAdapter, _scriptLoader);
+					_tokenHelper = new TokenisationHelper();
+				}
+			}
+		}
+
 		void IHttpHandler.ProcessRequest(HttpContext context)
 		{
-			IHttpContext contextAdaptor = new HttpContextAdapter(context);
-			var scriptLoader = new ScriptDependencyLoader(contextAdaptor);
-			scriptLoader.Initialise();
+			EnsureHandlersAreInitialised(context);
 
-			var tokenHelper = new TokenisationHelper();
-			var dependencies = tokenHelper.GetListOfDependencyNamesFromQueryStringTokens(context.Request.RawUrl,
-																						 scriptLoader.DependencyContainer);
+			var dependencies = _tokenHelper.GetListOfDependencyNamesFromQueryStringTokens(context.Request.RawUrl,
+																						 _scriptLoader.DependencyContainer);
 			var scriptType = dependencies[0].TypeOfScript;
 			var contentType = ScriptHelperConstants.ContentType_Javascript;
 			if (dependencies.Count > 0)
@@ -35,23 +58,23 @@ namespace ScriptDependencyExtension.Handler
 			}
 
 			string scriptToRender = null;
-			if (!contextAdaptor.IsDebuggingEnabled)
+			if (!_contextAdapter.IsDebuggingEnabled)
 			{
-				scriptToRender = contextAdaptor.GetItemFromGlobalCache<string>(context.Request.RawUrl);
+				scriptToRender = _contextAdapter.GetItemFromGlobalCache<string>(context.Request.RawUrl);
 			}
 			if (string.IsNullOrWhiteSpace(scriptToRender))
 			{
-				var engine = new ScriptEngine(contextAdaptor, scriptLoader);
+				//var engine = new ScriptEngine(contextAdaptor, scriptLoader);
 				var listOfFiles = dependencies.Select(d => d.ScriptPath).ToList();
-				var combiner = new FileCombiner(contextAdaptor, listOfFiles);
+				var combiner = new FileCombiner(_contextAdapter, listOfFiles);
 				StringBuilder contents = new StringBuilder(combiner.CombineFiles());
-				engine.ApplyFiltersToScriptOutput(contents, scriptType, scriptLoader.DependencyContainer);
+				_scriptEngine.ApplyFiltersToScriptOutput(contents, scriptType, _scriptLoader.DependencyContainer);
 				scriptToRender = contents.ToString();
 				
 				// We do an extra check here just in case the script has been added to the cachein between
 				// the last check and this one
-				if (string.IsNullOrWhiteSpace(contextAdaptor.GetItemFromGlobalCache<string>(context.Request.RawUrl)))
-					contextAdaptor.AddItemToGlobalCache(context.Request.RawUrl,scriptToRender);
+				if (string.IsNullOrWhiteSpace(_contextAdapter.GetItemFromGlobalCache<string>(context.Request.RawUrl)))
+					_contextAdapter.AddItemToGlobalCache(context.Request.RawUrl, scriptToRender);
 			}
 			context.Response.ContentType = contentType;
 			context.Response.Write(scriptToRender);
